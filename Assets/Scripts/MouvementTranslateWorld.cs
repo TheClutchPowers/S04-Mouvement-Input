b8829452835e1cfbb468b3014d07d7a00b66a0db
TheClutchPowers <dervieuxguillaume@gmail.com>
2025-02-12 16:45:56 -0500

    dollyyyyyyy

--- Assets/Scripts/MouvementTranslateWorld.cs
+++ Assets/Scripts/MouvementTranslateWorld.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class MouvementTranslateWorld : MonoBehaviour
 {
+    [SerializeField] private Vector3 _deplacement;
     void Update()
     {
-        transform.Translate(1f * Time.deltaTime, 0, 0, Space.World);
+        Vector3 deplacementCetFrame = _deplacement * Time.deltaTime;
+        transform.Translate(_deplacement.x, _deplacement.y, _deplacement.z, Space.Self);
 
         //Vector3 deplacement = new Vector3(1f, 0, 0);
         //transform.Translate(deplacement * Time.deltaTime, Space.World);
